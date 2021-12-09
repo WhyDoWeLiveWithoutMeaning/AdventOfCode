73d2096cb46f74caf41cbe13a0f18f34070da9b0
Eric Beaulne <eric.minecraft20@gmail.com>
2021-12-09 00:56:02 -0500

    0.0

--- y21/Day9/P1/Main.py
+++ y21/Day9/P1/Main.py
@@ -1,1 +1,27 @@
-data = open("y21\\Day9\\input.txt").read()
+input = open("y21\\Day9\\input.txt").read().splitlines()
+
+data = []
+for line in input:
+    data.append([int(i) for i in line])
+
+def cns(arr, x1, y1, x2, y2) -> bool:
+    try:
+        if x2 < 0 or y2 < 0:
+            raise IndexError
+        if arr[x1][y1] < arr[x2][y2]:
+            return True
+        else:
+            return False
+    except:
+        return True
+
+
+def get_lowspot_index(arr: list[list[int]]) -> list[(int,int)]:
+    lowspot = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if cns(arr, i, j, i-1, j) and cns(arr, i, j, i+1, j) and cns(arr, i, j, i, j-1) and cns(arr, i, j, i, j+1):
+                lowspot += 1 + data[i][j]
+    return lowspot
+
+print(get_lowspot_index(data))
--- y21/Day9/P2/Main.py
+++ y21/Day9/P2/Main.py
@@ -1,1 +1,9 @@
-data = open("y21\\Day9\\input.txt").read()
+input = open("y21\\Day9\\input.txt").read().splitlines()
+
+data = []
+for line in input:
+    data.append([int(i) for i in line])
+
+
+
+print()
